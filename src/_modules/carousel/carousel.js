4fd1e8d8cac06739c459cc34305cb6c2ce1936f9
Ajinkya Borade <ajinkyab1987@gmail.com>
2016-07-09 17:26:19 +0530

    added slideRight fn

--- src/_modules/carousel/carousel.js
+++ src/_modules/carousel/carousel.js
@@ -18,14 +18,18 @@ class Carousel {
 
     //init
     this.setUlWidth();
+    this.registerEvents();
+    this.timer();
+  }
 
+  timer() {
     this.slideInterval = window.setInterval(() => {
       this.slideLeft();
     }, 2000);
   }
 
   displayCount(){
-    this.controlsSpan.text("Current: " + (this.currentImage + 1));
+    this.controlsSpan.text("Current: " + (this.currentImage % this.slidesCount + 1));
   }
 
   setUlWidth(){
@@ -33,6 +37,7 @@ class Carousel {
   }
 
   slideLeft(){
+    window.clearTimeout(this.slideInterval);
     if(this.currentImage === this.slidesCount - 1) {
       this.ul.animate({
         "margin-left": 0
@@ -42,12 +47,46 @@ class Carousel {
       });
     } else {
       this.ul.animate({
-        "margin-left": "-=300px"
+        "margin-left": (this.currentImage % this.slidesCount + 1) * -300
       }, () => {
         this.currentImage+=1;
         this.displayCount();
       });
     }
+
+    this.timer();
+  }
+
+  slideRight(){
+    window.clearTimeout(this.slideInterval);
+    if(this.currentImage === 0) {
+      this.ul.animate({
+        "margin-left":  (this.slidesCount - 1) * -300
+      }, () => {
+        this.currentImage = 0;
+        this.displayCount();
+      });
+    } else {
+      this.ul.animate({
+        "margin-left": "+=300px"
+      }, () => {
+        this.currentImage += 1;
+        this.displayCount();
+      });
+    }
+    this.timer();
+  }
+
+  registerEvents() {
+    this.controlsLeft.on('click', (event) => {
+      event.preventDefault();
+      this.slideLeft();
+    });
+
+    this.controlsRight.on('click', (event) => {
+      event.preventDefault();
+      this.slideRight();
+    });
   }
 
 
